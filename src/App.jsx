4ed11735f094ccb569ef73b2b5c581a848200abb
Jhonray Acojedo <jhonray.acojedo@cit.edu>
2024-05-18 14:43:21 +0800

    Design landing and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import Nav from './components/navbar/index'
+import MEImage from './images/Mirrors-edge.jpg'
+import Footer from './components/footer/footer'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [hover, setHover] = useState(false);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Nav></Nav>
+    <div className='bg-black pt-[70px] w-full min-h-screen'>
+      <h1 className='text-white text-4xl text-center pt-10'>Welcome to Game Tracker</h1>
+      <div className="relative mx-auto my-10">
+          <img src={MEImage} alt="Mirrors edge image" className="w-full transition duration-500 ease-in-out transform hover:scale-105" />
+          <div className="absolute inset-0 bg-gray-800 opacity-0 hover:opacity-50 transition-opacity duration-500 flex flex-col items-center justify-center space-y-4">
+            <span className='text-white text-xl font-semibold'>Mirrors Edge Catalyst ™</span>
+            <button className="text-white px-4 py-2 rounded bg-black hover:bg-white hover:text-black focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+              Learn More
+            </button>
+          </div>
+        </div>
+    </div>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className={`h-[300px] bg-black bg-cover flex items-end`}>
+			<div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+				<div className="md:flex justify-center gap-x-40">
+					<div className="grid grid-cols gap-10 ">
+						<div>
+							<h2 className="mb-6 text-sm font-semibold text-white uppercase dark:text-white">About me</h2>
+							<ul className="text-white dark:text-white font-medium">
+								<li className="mb-4">
+									<a
+										href="#"
+										className="hover:underline"
+									>
+										Who are we?
+									</a>
+								</li>
+								<li>
+									<a
+										href="#"
+										className="hover:underline"
+									>
+										Team
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+                    <div className="grid gap-10">
+						<div>
+							<h2 className="mb-6 text-sm font-semibold text-white uppercase">Follow me</h2>
+							<ul className="text-white font-medium">
+								<li className="mb-4">
+									<a
+										href="https://facebook.com"
+										className="hover:underline "
+									>
+										Facebook
+									</a>
+								</li>
+								<li className="mb-4">
+									<a
+										href="https://github.com/JhonrayAcojedo/game-tracker"
+										className="hover:underline "
+									>
+										Github
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://linkedin.com"
+										className="hover:underline"
+									>
+										LinkedIn
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<hr className="my-6 border-white sm:mx-auto lg:my-8" />
+				<div className="sm:flex sm:items-center sm:justify-center">
+					<span className="text-sm text-white sm:text-center dark:text-white">
+						© 2024{' '}
+						<a
+							href="#"
+							className="hover:underline"
+						>
+							Jhonray Acojedo
+						</a>
+						. All Rights Reserved.
+					</span>
+				</div>
+			</div>
+		</footer>
+  )
+}
+
+export default Footer
