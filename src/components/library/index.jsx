5b85158f2d5a7b7008b5ffc771525ec484bc97d7
Jhonray Acojedo <jhonray.acojedo@cit.edu>
2024-05-19 18:27:03 +0800

    Center spinner

--- src/components/library/index.jsx
+++ src/components/library/index.jsx
@@ -32,13 +32,16 @@ const Library = () => {
         <br/>
 
       <div className='flex items-center justify-center'>
-        <div className='grid grid-cols-5 gap-x-16 px-10'>
-          { loading ? <Spinner/> :
-          (
-            games.map((game) => (
+      { loading ?
+          <div className='flex justify-center items-center h-full'>
+            <Spinner/>
+          </div> :
+          <div className='grid grid-cols-5 gap-x-16 px-10'>
+            {games.map((game) => (
               <Card key={game.id} game={game} onClick={() => {navigate(`/library/${game.id}`)}}/>
-          )))}
-        </div>
+            ))}
+          </div>
+        }
       </div>
     </div>
   )
